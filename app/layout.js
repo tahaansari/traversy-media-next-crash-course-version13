52ff9e50d5abcf4635c93ab7accc6fe7e8b0554d
Taha Ansari <taha.m.ansari@gmail.com>
2023-07-27 17:29:11 +0530

    wip

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,19 @@ export const metadata = {
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptate nisi nemo in hic veritatis natus eveniet ducimus sed illo.",
 };
 
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>
+        <main className="container">{children}</main>
+      </body>
     </html>
   );
 }
